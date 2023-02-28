3b7fe48c076978edfa951db9bd4da094757b302b
Arash Nemat Zadeh <arashnm80@gmail.com>
2023-02-28 04:03:15 +0330

    add log channel

--- variables.py
+++ variables.py
@@ -3,3 +3,4 @@ import os
 peapix_url = 'https://peapix.com/bing/feed?country=au'
 bot_url = "https://api.telegram.org/bot" + os.environ['BINGWALLS_BOT_API'] + "/"
 chat_id = os.environ['BING_WALLPAPERS_CHANNEL_USERNAME']
+log_channel_id = os.environ['LOG_CHANNEL_ID']
